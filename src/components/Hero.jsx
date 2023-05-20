b7710930bd8c538d24a42b302bbde0658a712736
labibs <visioner.lv@gmail.com>
2023-05-20 10:45:19 +0700

    menampilkan computer gltf

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,13 +8,16 @@ const Hero = () => {
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#5effbc]" />
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}> Hey, Saya <span className="text-[#5effbc]">Labib</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>Saya IT Helpdesk dan <br className="sm:block hidden" />IT Develop di JNE Cabang Cilacap</p>
         </div>
       </div>
+
+      <ComputersCanvas />
     </section>
   )
 }
