b011ca42a51c5520c77c25fbca5ff7c57fff1568
labibs <visioner.lv@gmail.com>
2023-05-20 16:57:25 +0700

    membuat komponen about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+// import Tilt from 'react-tilt'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constans';
+import { fadeIn, textVariant } from '../utils/motion';
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>PENGENALAN</p>
+        <h2 className={styles.sectionHeadText}>Gambaran Umum.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] '
+      >
+        Selamat datang di profil saya! Saya seorang lulusan S1 Teknik Informatika dengan pengalaman 3 tahun di bidang web development. Saya ahli dalam PHP, JavaScript, dan CSS, serta menggunakan framework seperti Laravel, React.js, dan Bootstrap.
+
+        Saya memiliki bakat dalam menciptakan pengalaman pengguna yang menarik dan responsif. Dalam karir saya, saya telah terlibat dalam proyek-proyek sukses mulai dari pembuatan website perusahaan hingga aplikasi web kompleks.
+
+        Saya sangat senang bekerja dalam tim kolaboratif dan memiliki kemampuan komunikasi yang kuat. Saya siap menghadapi tantangan baru dalam pengembangan web dan menciptakan solusi inovatif.
+
+        Terima kasih telah mengunjungi profil saya. Jika Anda tertarik untuk bekerja sama atau memiliki pertanyaan, jangan ragu untuk menghubungi saya. Saya siap untuk menciptakan pengalaman web yang menarik bersama Anda
+      </motion.p>
+    </>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,11 +13,30 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}> Hey, Saya <span className="text-[#5effbc]">Labib</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>Saya IT Helpdesk dan <br className="sm:block hidden" />IT Develop di JNE Cabang Cilacap</p>
+          <p className={`${styles.heroSubText} mt-2 mb-2 text-white-100`}>Saya IT Helpdesk dan <br className="sm:block hidden" />IT Develop di JNE Cabang Cilacap</p>
         </div>
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 top-[106%] w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop"
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+
+      </div>
     </section>
   )
 }
